fa2bcf93009b52d27ff45a0d3bd460cbae85db99
Rishirich4090 <Rishirich4090@users.noreply.github.com>
2025-08-18 16:48:08 +0530

    resolve the error

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      // Add browser support without aggressive grid features
+      overrideBrowserslist: [
+        '> 1%',
+        'last 2 versions',
+        'not dead'
+      ],
+      // Disable grid autoplacement to avoid warnings
+      grid: false
+    },
   },
 };
